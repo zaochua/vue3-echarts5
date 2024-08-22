ac0477cb66ad49bcf82bcbcc4509669b22da4114
prefers <1663955647@qq.com>
2024-08-22 10:34:57 +0800

    fix: 尝试切换git pages的后缀路径

--- demo-visualization/vite.config.js
+++ demo-visualization/vite.config.js
@@ -4,7 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/vue3-echarts5/",
+  // 这里用于打包厚放到github的时候的路径
+  base:"./",
   plugins: [vue()],
 
   // 别名
