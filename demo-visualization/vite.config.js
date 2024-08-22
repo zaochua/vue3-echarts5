1e96a84ae4d971454e7b5b1b2eb25cca955d69b0
prefers <1663955647@qq.com>
2024-08-22 12:22:12 +0800

    fix: 回滚

--- demo-visualization/vite.config.js
+++ demo-visualization/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   // 这里用于打包厚放到github的时候的路径
-  base:"/demo/",
+  base:"/vue3-echarts5/",
   plugins: [vue()],
 
   // 别名
